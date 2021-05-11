86d18d15ceda99270aa250c2612f0d2923e9625e
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-11 19:20:52 +0530

    display the sidebar chat component based on condditional prop

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
-  return (
+
+  const createChat = () => {};
+
+  return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar
         src={`https://avatars.dicebear.com/api/human/${seed}.svg`}
@@ -17,6 +20,10 @@ function SidebarChat() {
         <h2>Room Name</h2>
         <p>Last sent message...</p>
       </div>
+    </div>
+  ) : (
+    <div className="sidebarCh" onClick={createChat}>
+      <h2>Add New Chat</h2>
     </div>
   );
 }
