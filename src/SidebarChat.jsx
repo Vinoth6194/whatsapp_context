519fb3eb898bfaa74e20ce4eeadd1af53559a98e
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-11 18:32:42 +0530

    sidebar chats -css

---
+++ src/SidebarChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SidebarChat.css';
+function SidebarChat() {
+  return (
+    <div>
+      <h1>Sidebarchats</h1>
+    </div>
+  );
+}
+
+export default SidebarChat;
