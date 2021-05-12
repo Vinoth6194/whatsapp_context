be7d555264a121af82895f734aa0493372556b15
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-12 20:06:18 +0530

    logged the snapshot from chats

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -7,15 +7,24 @@ import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
 import InsertEmoticonOutlinedIcon from '@material-ui/icons/InsertEmoticonOutlined';
 import MicNoneOutlinedIcon from '@material-ui/icons/MicNoneOutlined';
 import { useParams } from 'react-router-dom';
+import db from './firebase';
 
 function Chat() {
   const [seed, setSeed] = useState('');
   const [input, setInput] = useState('');
   const { roomId } = useParams();
-
+  const [roomName, setRoomName] = useState();
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot(snapshot => console.log(snapshot.data()));
+    }
+  }, [roomId]);
 
   const sendMessage = e => {
     e.preventDefault();
@@ -29,7 +38,8 @@ function Chat() {
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerinfo">
-          <h2>Room name</h2>
+          {/* <h2>Room name</h2> */}
+          <h2>RoomName</h2>
           <p>Last seen...</p>
         </div>
         <div className="chat__headerRight">
