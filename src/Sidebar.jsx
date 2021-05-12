ac3c74fd1f7eafe7e263fe99ef0b2cffba825fa5
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-12 19:04:44 +0530

    logged the hardcoded rooms

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Sidebar.css';
 import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
@@ -6,9 +6,25 @@ import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import SidebarChat from './SidebarChat';
+import { useState } from 'react';
+import db from './firebase';
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot =>
+      setRooms(
+        snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
   return (
     <div className="sidebar">
+      {console.log('Rooms')}
+      {console.log(rooms)}
       <div className="sidebar__header">
         <Avatar />
         <div className="sidebar__headerRight">
