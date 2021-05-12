174422db9d4ecbd0d6aa05429bec01f57d14c659
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-12 22:58:40 +0530

    login -css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Chat from './Chat';
 import Sidebar from './Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useState } from 'react';
-
+import Login from './Login';
 function App() {
   const [user, setUser] = useState(null);
   return (
     <div className="app">
       {!user ? (
-        <h1>Login</h1>
+        <Login />
       ) : (
         <div className="app__body">
           <Router>
