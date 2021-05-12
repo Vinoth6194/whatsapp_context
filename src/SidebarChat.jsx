f3cdd1565b2aa0523d125f77a1c336c73a756b07
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-12 19:42:14 +0530

    enabled the rooms clickable with its own id

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 import Avatar from '@material-ui/core/Avatar';
 import db from './firebase';
+import { useParams, Link } from 'react-router-dom';
 
 function SidebarChat({ addNewChat, id, name }) {
   const [seed, setSeed] = useState('');
+  const { roomId } = useParams();
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -22,15 +24,17 @@ function SidebarChat({ addNewChat, id, name }) {
   };
 
   return !addNewChat ? (
-    <div className="sidebarChat">
-      <Avatar
-        src={`https://avatars.dicebear.com/api/human/${seed}.svg`}
-      ></Avatar>
-      <div className="sidebarChat_info">
-        <h2>{name}</h2>
-        <p>Last sent message...</p>
+    <Link to={`/rooms/${id}`}>
+      <div className="sidebarChat">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/human/${seed}.svg`}
+        ></Avatar>
+        <div className="sidebarChat_info">
+          <h2>{name}</h2>
+          <p>Last sent message...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   ) : (
     <div className="sidebarChat" onClick={createChat}>
       <h2>Add New Chat</h2>
