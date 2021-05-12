da489f19172359ab4711dac4cab8766399a37457
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-12 16:37:01 +0530

    header icons displayed

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,6 +1,9 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './Chat.css';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
+import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
 function Chat() {
   const [seed, setSeed] = useState('');
 
@@ -11,6 +14,21 @@ function Chat() {
     <div className="chat">
       <div className="div chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      </div>
+      <div className="chat__headerinfo">
+        <h2>Room name</h2>
+        <p>Last seen...</p>
+      </div>
+      <div className="chat__headerRight">
+        <IconButton>
+          <SearchOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <AttachFileOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertOutlinedIcon />
+        </IconButton>
       </div>
       <div className="div chat__body"></div>
       <div className="chat__footer"></div>
